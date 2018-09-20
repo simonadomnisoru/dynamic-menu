1bcd5e1725c981f075fdec089f5fe27d1c662b6f
Simona Domnisoru <simona.domnisoru@gmail.com>
2018-09-20 12:38:58 +0200

    Home styles

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Menu from './Menu';
+import Home from './Home';
+import '../style/buddle.css';
+
+const App = () => (
+    <div className="App">
+        <Home />
+    </div>
+);
+
+export default App;
---
+++ src/components/Menu.js
@@ -0,0 +1,18 @@
+import React from 'react';
+class Menu extends React.PureComponent {
+    render() {
+        return (
+            <div>
+                <ul>
+                    <li>WALLET</li>
+                    <li>ALL Coins</li>
+                    <li>Borrow</li>
+                    <li>Contacts</li>
+                    <li>Settings</li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default Menu;
